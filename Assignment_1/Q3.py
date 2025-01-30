3da0cb23cfcd35d80767d5f1ec7675eeb3ae952e
Russel Abreo <russela@nseit.com>
2025-01-30 15:07:20 +0530

    completed Q3

--- Assignment_1/Q3.py
+++ Assignment_1/Q3.py
@@ -1,6 +1,8 @@
-from scipy.io import loadmat
+# from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import numpy as np
+import mat73
+import os
 
 from config import *
 from model import *
@@ -11,12 +13,17 @@ def display_image(image):
 	plt.show()
 	# print(image)
 
+def save_image(directory, image, filename):
+	# plt.imshow(image, cmap=None)
+	plt.imshow((image * 255).astype(np.int32))
+	plt.savefig(os.path.join(directory, f'{filename}.png'))
+
 def normalize_image(image):
 	image = np.array(image)
 	return ( image - np.min(image) ) / ( np.max(image) - np.min(image) )
 
 
-def optimize(x_true ,y_observed, step_size=1e-2, iterations=150, alpha=0.5, gamma=0, likelihood="gaussian", prior="huber"):
+def optimize(x_true ,y_observed, step_size=1e-2, iterations=100, alpha=0.5, gamma=0, likelihood="gaussian", prior="huber"):
 
 	max_threshold = 2.5
 	min_threshold = 0.001
@@ -49,10 +56,10 @@ def optimize(x_true ,y_observed, step_size=1e-2, iterations=150, alpha=0.5, gamm
 		# 	print(f"Keeping step size same")
 
 
-		if new_log_posterior/initial_log_posterior > 1:
-			step_size *= 1.1
-		else:
-			step_size *= 0.5
+		# if new_log_posterior/initial_log_posterior > 1:
+		# 	step_size *= 1.1
+		# else:
+		# 	step_size *= 0.5
 
 		initial_log_posterior = new_log_posterior.copy()
 
@@ -60,7 +67,7 @@ def optimize(x_true ,y_observed, step_size=1e-2, iterations=150, alpha=0.5, gamm
 		
 		current_rrmse = rrmse(x_true, x_estimate)
 
-		# print(f"Iteration: {it}/{iterations} => Log Posterior: {np.sum(new_log_posterior):.4f}, RRMSE: {current_rrmse:.4f}")
+		print(f"Iteration: {it}/{iterations} => Log Posterior: {np.sum(new_log_posterior):.4f}, RRMSE: {current_rrmse:.4f}")
 		
 		log_posterior_grad = new_log_posterior_grad
 
@@ -68,8 +75,10 @@ def optimize(x_true ,y_observed, step_size=1e-2, iterations=150, alpha=0.5, gamm
 		if step_size <= 1e-8 : break
 
 
-	print(f"Log Posterior: {np.sum(new_log_posterior):.4f}, RRMSE: {current_rrmse:.4f} Alpha: {alpha} Gamma: {gamma} Prior: {prior}")
-	
+	print(x_estimate)
+	print(f"Log Posterior: {new_log_posterior:.4f}, RRMSE: {current_rrmse:.4f} Alpha: {alpha} Gamma: {gamma} Prior: {prior}")
+	print(f"Saving the denoised image to => {os.path.join("images/" , f'{prior}-denoised')}")
+	save_image("images/",x_estimate, f"{prior}-denoised")
 	return x_estimate
 
 
@@ -93,16 +102,13 @@ def grid_search(gamma_start = 0, gamma_end=0, gamma_increment = 1, alpha_start=0
 
 if __name__ == "__main__":
 	
-	phantom = loadmat('data/assignmentImageDenoising_phantom.mat')
-	imageNoiseless = phantom['imageNoiseless']
-	imageNoisy = phantom['imageNoisy']
+	microscopy = mat73.loadmat('data/assignmentImageDenoising_microscopy.mat')
+
+	imageNoiseless = microscopy['microscopyImageOrig']
+	imageNoisy = microscopy['microscopyImageNoisyScale350sigma0point06']
 	print(f'Image Size : {imageNoisy.shape}')
 
 	imageNoiseless, imageNoisy = normalize_image(imageNoiseless), normalize_image(imageNoisy)
 
-	# optimize(imageNoiseless, imageNoisy, alpha=0.0875, gamma=0.5, likelihood="gaussian", prior="huber")
-	grid_search(gamma_start = 0, gamma_end=4, gamma_increment = 0.5, alpha_start=0, alpha_end=1, alpha_increment=0.05, prior="huber", likelihood="gaussian")
-
-
-
+	optimize(imageNoiseless, imageNoisy, alpha=0.08, gamma=0.5, likelihood="gaussian", prior="huber-l1")
 
--- Assignment_1/model.py
+++ Assignment_1/model.py
@@ -101,6 +101,79 @@ def calc_adaptive_prior(x, gamma=0):
 
 
 
+
+def calc_square_l2_prior(x, gamma=None):
+
+	up, down, right, left = get_clique_differences(x)
+
+	left_prior = np.sum(np.square(left - x))
+	right_prior = np.sum(np.square(right - x))
+	up_prior = np.sum(np.square(up - x))
+	down_prior = np.sum(np.square(down - x))
+
+	left_prior_grad = 2 * (left - x)  
+	right_prior_grad = 2 * (right - x) 
+	up_prior_grad = 2 * (up - x) 
+	down_prior_grad = 2 * (down - x) 
+
+	prior = - (left_prior + right_prior + up_prior + down_prior)
+	prior_grad =  (left_prior_grad + right_prior_grad + up_prior_grad + down_prior_grad)
+
+	return prior, prior_grad
+
+
+def calc_l2_prior(x, gamma=None):
+	epsilon = 1e-8
+	up, down, right, left = get_clique_differences(x)
+
+	left_prior = np.sum(np.sqrt(np.square(left - x)))
+	right_prior = np.sum(np.sqrt(np.square(right - x)))
+	up_prior = np.sum(np.sqrt(np.square(up - x)))
+	down_prior = np.sum(np.sqrt(np.square(down - x)))
+
+	left_prior_grad = (left - x) * (1 / (np.sqrt(np.square(left - x) + epsilon)))  
+	right_prior_grad = (right - x) * (1 / (np.sqrt(np.square(right - x) + epsilon))) 
+	up_prior_grad = (up - x) * (1 / (np.sqrt(np.square(up - x) + epsilon))) 
+	down_prior_grad = (down - x) * (1 / (np.sqrt(np.square(down - x) + epsilon))) 
+	
+	prior = - (left_prior + right_prior + up_prior + down_prior)
+	prior_grad =  (left_prior_grad + right_prior_grad + up_prior_grad + down_prior_grad)
+
+	return prior, prior_grad
+
+
+def calc_huber_l1_prior(x, gamma=0):
+	up, down, right, left = get_clique_differences(x)
+
+	def huber(l1_norm):
+		return np.where(l1_norm <= gamma, 0.5 * (l1_norm ** 2), gamma * l1_norm - 0.5 * (gamma ** 2))
+	
+	def huber_grad(diff, l1_norm):
+		return np.where(l1_norm <= gamma, l1_norm * np.sign(diff), gamma * np.sign(diff))
+	
+	
+	l1_norm_left = np.sum(np.abs(left - x))
+	l1_norm_right = np.sum(np.abs(right - x))
+	l1_norm_up = np.sum(np.abs(up - x))
+	l1_norm_down = np.sum(np.abs(down - x))
+
+	left_prior_grad = huber_grad(left - x ,l1_norm_left)
+	right_prior_grad = huber_grad(right - x ,l1_norm_right)
+	up_prior_grad = huber_grad(up - x ,l1_norm_up)
+	down_prior_grad = huber_grad(down - x ,l1_norm_down)
+
+	prior = - (huber(l1_norm_left) + huber(l1_norm_right) + huber(l1_norm_up) + huber(l1_norm_down))
+	prior_grad =  (left_prior_grad + right_prior_grad + up_prior_grad + down_prior_grad)
+
+	return prior, prior_grad
+
+
+##############
+# Posterior
+##############
+
+
+
 def calculate_posterior(x, y, alpha=0.5, gamma = 0, likelihood="gaussian", prior="quadratic"):
 	# prior, prior_grad = calc_quadratic_prior(x)
 	# prior, prior_grad = calc_huber_prior(x, gamma)
@@ -112,7 +185,10 @@ def calculate_posterior(x, y, alpha=0.5, gamma = 0, likelihood="gaussian", prior
     prior_mapping = {
 		"quadratic":calc_quadratic_prior,
 		"huber": calc_huber_prior,
-		"adaptive":calc_adaptive_prior
+		"adaptive":calc_adaptive_prior,
+		"square-l2": calc_square_l2_prior,
+		"l2": calc_l2_prior,
+		"huber-l1": calc_huber_l1_prior
     }
 	
     likelihood_fn = likelihood_mapping[likelihood]
@@ -124,7 +200,7 @@ def calculate_posterior(x, y, alpha=0.5, gamma = 0, likelihood="gaussian", prior
     log_posterior = alpha*(prior) + (1 - alpha)*likelihood
     log_posterior_grad = alpha*(prior_grad) + (1 - alpha)*likelihood_grad
 	
-    print(log_posterior)
+    # print(log_posterior)
 
     return log_posterior, log_posterior_grad
 
@@ -132,8 +208,4 @@ def calculate_posterior(x, y, alpha=0.5, gamma = 0, likelihood="gaussian", prior
 def rrmse(A,B):
 	rrmse = np.sqrt(np.sum(np.square(np.abs(A)-np.abs(B)))/np.sum(A**2))
 	return rrmse
-
-	
-def grid_search(gamma_end, prior="quadratic" ,alpha_start=0, alpha_end=1, alpha_increment=0.1, gamma_start=0):
-    pass
 
