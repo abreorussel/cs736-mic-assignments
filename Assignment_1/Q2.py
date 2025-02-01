2df38934c4214107c8d42a0edfb682e8ab20d37b
Russel Abreo <russela@nseit.com>
2025-02-01 17:33:22 +0530

    changes

--- Assignment_1/Q2.py
+++ Assignment_1/Q2.py
@@ -12,11 +12,6 @@ def display_image(image):
 	plt.show()
 	# print(image)
 
-def normalize_image(image):
-	image = np.array(image)
-	return ( image - np.min(image) ) / ( np.max(image) - np.min(image) )
-
-
 def optimize(x_true ,y_observed, step_size=1e-2, iterations=200, alpha=0.5, gamma=0, likelihood="gaussian", prior="quadratic", print_log = True):
 	# print(f"ALpha : {alpha}")
 	max_threshold = 2.5
@@ -27,12 +22,10 @@ def optimize(x_true ,y_observed, step_size=1e-2, iterations=200, alpha=0.5, gamm
 	decrease_factor = 0.5
 	
 	x_estimate = y_observed.copy()
-	log_posterior_values = []
-	rrmse_values = list()
+	log_posterior_values = list()
 	
 	# Initial calculation
 	initial_log_posterior, log_posterior_grad = calculate_posterior(x_estimate, y_observed, alpha, gamma, likelihood=likelihood, prior=prior)
-	log_posterior_values.append(initial_log_posterior)
 	if print_log:
 		print("_________________________________________________________________________________")
 		print(f"\nAlpha: {alpha:.4f} | Gamma: {gamma:.4f} | Prior: {prior} | Likelihood: {likelihood}")
@@ -63,14 +56,13 @@ def optimize(x_true ,y_observed, step_size=1e-2, iterations=200, alpha=0.5, gamm
 		
 		log_posterior_grad = new_log_posterior_grad
 
-		if it == iterations: rrmse_values.append(current_rrmse)
 		if step_size <= 1e-8 : break
 
 	if print_log:
 		print(f"Post Denoising RRMSE:  {current_rrmse:.4f}")
 		print("_________________________________________________________________________________")
 	
-	return x_estimate, new_log_posterior, current_rrmse
+	return x_estimate, new_log_posterior, current_rrmse, log_posterior_values
 
 
 	
@@ -79,19 +71,19 @@ def grid_search(imageNoiseless, imageNoisy, gamma_start = 0, gamma_end=0, alpha_
 	alpha_optimal = 0
 	gamma_optimal = 0
 	gamma = 0.16
-	# alpha = 0.4371
+	# alpha = 0.6482
 
-	for alpha in np.linspace(alpha_start, alpha_end, 200):
+	for alpha in np.linspace(alpha_start, alpha_end, 500):
 	# for gamma in np.linspace(gamma_start, gamma_end, 200):
 		if mode != "optimize":
 			for gamma in np.linspace(gamma_start, gamma_end, 10):
-				x_estimate, new_log_posterior, current_rrmse = optimize(imageNoiseless, imageNoisy, alpha = alpha, gamma = gamma, likelihood = likelihood, prior = prior)
+				x_estimate, new_log_posterior, current_rrmse,_ = optimize(imageNoiseless, imageNoisy, alpha = alpha, gamma = gamma, likelihood = likelihood, prior = prior)
 				if current_rrmse < rrmse:
 					rrmse = current_rrmse
 					alpha_optimal = alpha
 					gamma_optimal = gamma
 		else:
-			x_estimate, new_log_posterior, current_rrmse = optimize(imageNoiseless, imageNoisy, alpha = alpha, gamma = gamma, likelihood = likelihood, prior = prior)
+			x_estimate, new_log_posterior, current_rrmse,_ = optimize(imageNoiseless, imageNoisy, alpha = alpha, gamma = gamma, likelihood = likelihood, prior = prior)
 			if current_rrmse < rrmse:
 				rrmse = current_rrmse
 				alpha_optimal = alpha
@@ -99,24 +91,27 @@ def grid_search(imageNoiseless, imageNoisy, gamma_start = 0, gamma_end=0, alpha_
 	
 
 	print(f"############################# Optimal Values #################################")
-	print(f'prior : {prior} | alpha : {alpha_optimal} | gamma : {gamma_optimal} | log posterior : {new_log_posterior} | RRMSE : {rrmse}')
+	print(f'prior : {prior} | alpha : {alpha_optimal:.4f} | gamma : {gamma_optimal:.4f} | log posterior : {new_log_posterior:.4f} | RRMSE : {rrmse:.4f}')
 
 def check_nearby_parameters(alpha =0 , gamma = 0, prior ="quadratic"):
+	increased_alpha = alpha
+	if 1.2 * alpha < 1:
+		increased_alpha = alpha * 1.2
 	if prior == "quadratic":
-		_, _, rrmse_optimal = optimize(imageNoiseless, imageNoisy, alpha=1.2*alpha, gamma=gamma, likelihood="gaussian", prior=prior ,print_log =False)
-		_, _, rrmse1 = optimize(imageNoiseless, imageNoisy, alpha=1.2*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
-		_, _, rrmse2 = optimize(imageNoiseless, imageNoisy, alpha=0.8*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse_optimal, _ = optimize(imageNoiseless, imageNoisy, alpha=alpha, gamma=gamma, likelihood="gaussian", prior=prior ,print_log =False)
+		_, _, rrmse1, _ = optimize(imageNoiseless, imageNoisy, alpha=increased_alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse2, _ = optimize(imageNoiseless, imageNoisy, alpha=0.8*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
 		
 		print(f"prior : {prior} | alpha : {alpha}")
 		print(f"Optimal RRMSE : {rrmse_optimal} ")
 		print(f"1.2 times alpha RRMSE : {rrmse1} ")
 		print(f"0.8 times alpha RRMSE : {rrmse2} ")
 	else:
-		_, _, rrmse_optimal = optimize(imageNoiseless, imageNoisy, alpha=1.2*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
-		_, _, rrmse1 = optimize(imageNoiseless, imageNoisy, alpha=1.2*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
-		_, _, rrmse2 = optimize(imageNoiseless, imageNoisy, alpha=0.8*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
-		_, _, rrmse3 = optimize(imageNoiseless, imageNoisy, alpha=alpha, gamma=1.2*gamma, likelihood="gaussian", prior=prior, print_log=False)
-		_, _, rrmse4 = optimize(imageNoiseless, imageNoisy, alpha=alpha, gamma=0.8*gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse_optimal, _ = optimize(imageNoiseless, imageNoisy, alpha=alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse1, _ = optimize(imageNoiseless, imageNoisy, alpha=increased_alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse2, _ = optimize(imageNoiseless, imageNoisy, alpha=0.8*alpha, gamma=gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse3, _ = optimize(imageNoiseless, imageNoisy, alpha=alpha, gamma=1.2*gamma, likelihood="gaussian", prior=prior, print_log=False)
+		_, _, rrmse4, _ = optimize(imageNoiseless, imageNoisy, alpha=alpha, gamma=0.8*gamma, likelihood="gaussian", prior=prior, print_log=False)
 
 		print(f"prior : {prior} | alpha : {alpha} | gamma : {gamma}")
 		print(f"Optimal RRMSE : {rrmse_optimal} ")
@@ -126,6 +121,29 @@ def check_nearby_parameters(alpha =0 , gamma = 0, prior ="quadratic"):
 		print(f"0.8 times gamma RRMSE : {rrmse4} ")
 
 
+def save_image(directory, image, filename):
+	plt.imshow(image, cmap="jet")
+	# plt.imshow((image * 255).astype(np.int32))
+	plt.grid(False)
+	plt.savefig(os.path.join(directory, f'{filename}.png'))
+
+def save_all_images(imageNoiseless, imageNoisy, x_estimate_quadratic, x_estimate_huber, x_estimate_adaptive):
+	save_image(results_folder, imageNoiseless,  "image_noiseless")
+	save_image(results_folder, imageNoisy,  "image_noisy")
+	save_image(results_folder, x_estimate_quadratic,  "x_estimate_quadratic")
+	save_image(results_folder, x_estimate_huber,  "x_estimate_huber")
+	save_image(results_folder, x_estimate_adaptive,  "x_estimate_adaptive")
+
+def construct_graph(iterations, function_values, title, filename, directory):
+	# print(function_values)
+	plt.figure(figsize=(8, 5))
+	plt.plot(list(range(1,iterations+1)), function_values, marker='o', linestyle='-', color='b', markersize=4)
+	plt.xlabel("Iterations")
+	plt.ylabel("Objective Function Value")
+	plt.title(title)
+	plt.grid(True)
+	# plt.show()
+	plt.savefig(os.path.join(directory, f'{filename}.png'))
 
 if __name__ == "__main__":
 	
@@ -135,16 +153,31 @@ if __name__ == "__main__":
 	imageNoisy = brain_mri['brainMRIsliceNoisy']
 	print(f'Image Size : {imageNoisy.shape}')
 
-	results_folder = "results/Q1"
+	results_folder = "results/Q2"
 	if not os.path.exists(results_folder):
 		os.makedirs(results_folder)
 		print(f'Folder "{results_folder}" created.')
 
-	optimize(imageNoiseless, imageNoisy, alpha=0.142285, gamma=0.04, likelihood="gaussian", prior="quadratic")
+	# optimize(imageNoiseless, imageNoisy, alpha=0.142285, gamma=0.04, likelihood="gaussian", prior="quadratic")
 	# grid_search(imageNoiseless, imageNoisy, gamma_start = 1, gamma_end=10, alpha_start=0, alpha_end=1, prior="huber", likelihood="gaussian")
 	# grid_search(imageNoiseless, imageNoisy, gamma_start = 1, gamma_end=10, alpha_start=0, alpha_end=1, prior="huber", likelihood="gaussian")
 
 
-	check_nearby_parameters(alpha=0.142284, gamma=0, prior="quadratic") 
-	check_nearby_parameters(alpha=0.47034719, gamma=0.064320, prior="huber") 
-	check_nearby_parameters(alpha=0.5607599, gamma=0.0723584, prior="adaptive")
+	# check_nearby_parameters(alpha=0.142284, gamma=0, prior="quadratic") 
+	# check_nearby_parameters(alpha=0.47034719, gamma=0.064320, prior="huber") 
+	# check_nearby_parameters(alpha=0.5607599, gamma=0.0723584, prior="adaptive")
+
+	# Best Estimates
+	x_estimate_quadratic, new_log_posterior_quadratic, current_rrmse_quadratic, log_posterior_values_quadratic = optimize(imageNoiseless, imageNoisy, alpha=0.142284, gamma=0, likelihood="gaussian", prior="quadratic", print_log=False)
+	x_estimate_huber, new_log_posterior_huber, current_rrmse_huber, log_posterior_values_huber = optimize(imageNoiseless, imageNoisy, alpha=0.47034719, gamma=0.064320, likelihood="gaussian", prior="huber", print_log=False)
+	x_estimate_adaptive, new_log_posterior_adaptive, current_rrmse_adaptive, log_posterior_values_adaptive = optimize(imageNoiseless, imageNoisy, alpha=0.5607599, gamma=0.0723584, likelihood="gaussian", prior="adaptive", print_log=False)
+	
+	
+	save_all_images(imageNoiseless, imageNoisy, x_estimate_quadratic, x_estimate_huber, x_estimate_adaptive)
+
+	construct_graph(iterations=200, function_values=log_posterior_values_quadratic, title="Objective Function vs Iterations : Quadratic", filename="quadratic_plot", directory=results_folder )
+	construct_graph(iterations=200, function_values=log_posterior_values_huber, title="Objective Function vs Iterations : Discontinuity-adaptive Huber", filename="huber_plot", directory=results_folder )
+	construct_graph(iterations=200, function_values=log_posterior_values_adaptive, title="Objective Function vs Iterations : Discontinuity-adaptive", filename="adaptive_plot", directory=results_folder )
+
+
+	
